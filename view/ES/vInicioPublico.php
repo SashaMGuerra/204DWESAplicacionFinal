282cc7954c86c9bae0623e927338693c654b2d5a
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2022-01-28 10:17:28 +0100

    28/01/2022 Diagramas

--- view/ES/vInicioPublico.php
+++ view/ES/vInicioPublico.php
@@ -18,8 +18,30 @@
                 </form>
             </div>
         </div>
+        <section>Práctica final de creación de una aplicación multicapa, basada en el paradigma Modelo-Vista-Controlador, y orientada a objetos.</section>
         <section>
-            <div>Práctica final de creación de una aplicación multicapa, basada en el paradigma Modelo-Vista-Controlador, y orientada a objetos.</div>
+            <article>
+                <a href="webroot/files/220119CatalogoDeRequisitos.pdf" target="_blank">Catálogo de requisitos</a>
+            </article>
+            <article>
+                <a href="webroot/files/220119DiagramaDeCasosDeUso.pdf" target="_blank">Diagrama de casos de uso</a>
+            </article>
+            <article>
+                <a href="webroot/files/220119DiagramaDeClases.pdf" target="_blank">Diagrama de clases</a>
+            </article>
+            <article>
+                <a href="webroot/files/220119ArbolDeNavegación.pdf" target="_blank">Árbol de navegación</a>
+            </article>
+            <article>
+                <a href="webroot/files/220119RelacionDeFicheros.pdf" target="_blank">Mapa web - relación de ficheros</a>
+            </article>
+            <article>
+                <a href="webroot/files/211129EstandarDesarrolloDAWyEstructuraAlmacenamientoDWES.pdf" target="_blank">Estructura de almacenamiento</a>
+            </article>
+            <article>
+                <a href="webroot/files/201127ModeloFisicoDeDatos20-21.pdf" target="_blank">Modelo físico de datos</a>
+            </article>
         </section>
+
 </main>
 <div class="background"></div>
